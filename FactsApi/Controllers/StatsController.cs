d6055293ea5d0e2536d4141199e1dbd5dc360193
seferlisk <seferlisk@gmail.com>
2025-02-17 17:12:52 +0200

    created StatsController

---
+++ FactsApi/Controllers/StatsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FactsApi.Controllers
+{
+    [ApiController]
+    [Route("api/stats")]
+    public class StatsController : ControllerBase
+    {
+        private readonly ApiStatisticsService _apiStatisticsService;
+
+        public StatsController(ApiStatisticsService apiStatisticsService)
+        {
+            _apiStatisticsService = apiStatisticsService;
+        }
+
+        [HttpGet]
+        public IActionResult GetStatistics()
+        {
+            var stats = _apiStatisticsService.GetStatistics();
+            return Ok(stats);
+        }
+    }
+}
